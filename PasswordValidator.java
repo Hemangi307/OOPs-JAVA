1bb568ca41efce924b6e00179dce4b0522f118c2
Hemangi307 <hemangichavda29@gmail.com>
2026-03-10 00:21:58 +0530

    Add files via upload

---
+++ PasswordValidator.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class PasswordValidator {
+
+    // Method to check if password is valid
+    public static boolean isValidPassword(String password) {
+
+        // Rule 1: At least 8 characters
+        if (password.length() < 8) {
+            return false;
+        }
+
+        int digitCount = 0;
+
+        // Check each character
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            // Rule 2: Only letters and digits allowed
+            if (!Character.isLetterOrDigit(ch)) {
+                return false;
+            }
+
+            // Count digits
+            if (Character.isDigit(ch)) {
+                digitCount++;
+            }
+        }
+
+        // Rule 3: At least two digits
+        if (digitCount < 2) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        
+
+        System.out.println("240390107011");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+
+        if (isValidPassword(password)) {
+            System.out.println("Valid Password");
+        } else {
+            System.out.println("Invalid Password");
+        }
+
+        sc.close();
+    }
+}
